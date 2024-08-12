8e25388fafcea79992422baaf0e5925d8cafa3de
Michael ADDA <michael.adda92@gmail.com>
2024-08-12 22:34:57 +0200

    PLEIN DE TRUCS

--- accueil.js
+++ accueil.js
@@ -1,13 +1,44 @@
-document.getElementById('bouton-1').addEventListener('click', function() {
-    // Afficher le carré bleu
-    var square = document.getElementById('illu-1');
-    square.style.display = 'block';
+function afficherCarre(numero) {
+   
+    document.getElementById('illu-1').style.display = 'none';
+    document.getElementById('illu-2').style.display = 'none';
+    document.getElementById('illu-3').style.display = 'none';
+    document.getElementById('illu-4').style.display = 'none';
+    document.getElementById('illu-5').style.display = 'none';
     
-    // Retarder le défilement pour s'assurer que le carré est visible
+
+
+    var square = document.getElementById('illu-' + numero);
+    square.style.display = 'block';
+
     setTimeout(function() {
         window.scrollTo({
-            top: document.body.scrollHeight,
+            top: square.offsetTop,
             behavior: 'smooth'
         });
-    }, 100); // Petit délai pour s'assurer que le carré est bien rendu avant de faire défiler
-});
+    }, 100); 
+}
+
+afficherCarreEtDefiler('bouton-1', 'illu-1');
+afficherCarreEtDefiler('bouton-2', 'illu-2');
+afficherCarreEtDefiler('bouton-3', 'illu-3');
+afficherCarreEtDefiler('bouton-4', 'illu-4');
+afficherCarreEtDefiler('bouton-5', 'illu-5');
+
+
+function agrandirImage(src) {
+    var modal = document.getElementById("modal");
+    var imageAgrandie = document.getElementById("image-agrandie");
+
+    // Met à jour la source de l'image affichée dans le modal
+    imageAgrandie.src = src;
+
+    // Affiche le modal
+    modal.style.display = "block";
+}
+
+// Fonction pour fermer le modal
+function fermerModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
